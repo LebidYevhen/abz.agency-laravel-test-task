d1f50088e931fa1db42319e85f18ddf3b463fa53
yevhen <yevhenlebid51@gmail.com>
2024-03-17 20:31:42 +0200

    Implement route for positions

---
+++ app/Http/Controllers/Api/PositionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PositionCollection;
+use App\Http\Resources\PositionResource;
+use App\Models\Position;
+use Illuminate\Http\Request;
+
+class PositionController extends Controller
+{
+    public function show()
+    {
+        $positions = Position::all();
+
+        if (!$positions) {
+            return response()->json([
+              'success' => false,
+              'message' => 'Positions not found'
+            ], 422);
+        }
+
+        return response()->json([
+          'success' => true,
+          'positions' => new PositionCollection($positions),
+        ]);
+    }
+}
