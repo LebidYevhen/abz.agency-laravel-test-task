3eaa9990521ea9f1155b1b04a842f094495b2ef4
yevhen <yevhenlebid51@gmail.com>
2024-03-17 20:41:20 +0200

    Implement force json response and add fallback route

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\Api\PositionController;
-use App\Http\Resources\PositionCollection;
-use App\Models\Position;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::fallback(function () {
+    return response()->json([
+      'success' => false,
+      'message' => 'Page not found'
+    ], 404);
+});
 
 Route::prefix('v1')->group(function () {
     Route::get('/positions', [PositionController::class, 'show']);
